5608e61ee18eb0061534226ea864664660cce206
Sabrina <sabrina.cloud14@gmail.com>
2021-11-17 13:42:54 +0800

    Add Image extension

--- Sources/SJUtil/Extension/UIImage+Extension.swift
+++ Sources/SJUtil/Extension/UIImage+Extension.swift
@@ -38,4 +38,12 @@ public extension UIImage {
         UIGraphicsEndImageContext()
         return img
     }
+    
+    func image(alpha: CGFloat) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        draw(at: .zero, blendMode: .normal, alpha: alpha)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
 }
