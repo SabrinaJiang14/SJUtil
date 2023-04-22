3e273b95a70d772881b2ad3520896e12efa40d10
SabrinaJiang <j.sabrina0326@gmail.com>
2023-04-22 12:17:22 +0800

    Update swift version 5.3 to 5.7

--- Tests/SJUtilTests/SJUtilTests.swift
+++ Tests/SJUtilTests/SJUtilTests.swift
@@ -1,10 +1,11 @@
-    import XCTest
-    @testable import SJUtil
+import XCTest
+@testable import SJUtil
 
-    final class SJUtilTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-        }
+final class SJUtilTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SJUtil().text, "Hello, World!")
     }
+}
